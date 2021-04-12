0910b80a69fc5a7ae1c3231a2d292be53f8b9737
Nasib Sarvari <NSV-ADMIN@energinet.local>
2021-04-12 12:09:13 +0200

    added metrics: guid to egress

--- app/routers/downloads.py
+++ app/routers/downloads.py
@@ -45,9 +45,11 @@ async def download_json_file(guid: str,
         directory_client = filesystem_client.get_directory_client(guid)
         __check_directory_exist(directory_client)
         path = f'year={file_date.year:02d}/month={file_date.month:02d}/day={file_date.day:02d}/data.json'
+        print(type(file_date), file_date, path)
         stream = __download_file(path, directory_client)
 
         return StreamingResponse(stream.chunks(), media_type='application/octet-stream')
+    
 
 
 @router.get('/{guid}', response_class=StreamingResponse)
