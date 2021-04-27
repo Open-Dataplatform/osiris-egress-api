86e3c99dec2f1a7fedfa64cd91c264926bc064f8
runethorbek <runethorbek@gmail.com>
2021-04-27 13:19:57 +0200

    temp save

--- app/routers/downloads.py
+++ app/routers/downloads.py
@@ -5,12 +5,14 @@ import os
 from http import HTTPStatus
 from datetime import datetime, date
 
+from dateutil.relativedelta import relativedelta
 from fastapi import APIRouter, HTTPException, Security
 from fastapi.security.api_key import APIKeyHeader
 from fastapi.responses import StreamingResponse
 
 from azure.core.exceptions import ResourceNotFoundError
 from azure.storage.filedatalake import DataLakeDirectoryClient, FileSystemClient, StorageStreamDownloader
+import asyncio
 from osiris.core.azure_client_authorization import AzureCredential
 
 from ..dependencies import Configuration, Metric, TracerClass
@@ -80,17 +82,36 @@ async def download_file(guid: str,
 @router.get('/{guid}/jao', response_class=StreamingResponse)
 @Metric.histogram
 async def download_jao_data(guid: str,
-                            from_date: date = datetime.utcnow().date(),
+                            from_date: date,
                             to_date: date = datetime.utcnow().date(),
                             token: str = Security(access_token_header)) -> StreamingResponse:
+    """
+    Download JAO data endpoint with data from from_date to to_date
+    """
+    async def download(fetch_date: date):
+        path = __get_path_for_generic_file(from_date, guid, filesystem_client)
+        stream = __download_file(path, directory_client)
+
+        return stream
+
     logger.debug('download jao data requested')
+
+    print(from_date, type(from_date))
+    print(to_date, type(to_date))
 
     with __get_filesystem_client(token) as filesystem_client:
         directory_client = filesystem_client.get_directory_client(guid)
         __check_directory_exist(directory_client)
+        fetch_date = from_date.replace(day=1)
+        # We store data the first every month - as data does not change
+        fetch_dates = [fetch_date]
+        while fetch_date < to_date:
+            fetch_date += relativedelta(months=+1)
+            fetch_dates.append(fetch_date)
         path = __get_path_for_generic_file(from_date, guid, filesystem_client)
         stream = __download_file(path, directory_client)
 
+    # foo = await asyncio.gather(*[download(fetch_date) for fetch_date in fetch_dates])
     return StreamingResponse(stream.chunks(), media_type='application/octet-stream')
 
 
